0aa0beb17f7172b24a10a15cf1a2fd8564fdb723
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-23 17:59:35 +0300

    User Serializer

--- backend/base/views.py
+++ backend/base/views.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Product
 from .products import products
-from .serailizers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 # Create your views here.
 
@@ -16,8 +16,10 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
   def validate(self, attrs):
     data = super().validate(attrs)
 
-    data['username'] = self.user.username
-    data['email'] = self.user.email
+    serializer = UserSerializerWithToken(self.user).data
+
+    for k, v in serializer.items():
+      data[k] = v
 
     return data
 
@@ -47,15 +49,22 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+def getUserProfile(request):
+  user = request.user
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
+
+
+@api_view(['GET'])
 def getProducts(request):
   products = Product.objects.all()
-  serailizer = ProductSerializer(products, many=True)
+  serializer = ProductSerializer(products, many=True)
   # return JsonResponse(products, safe=False)
-  return Response(serailizer.data)
+  return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
   product = Product.objects.get(_id=pk)
-  serailizer = ProductSerializer(product, many=False)
-  return Response(serailizer.data)
+  serializer = ProductSerializer(product, many=False)
+  return Response(serializer.data)
