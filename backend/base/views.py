a4626467938f60bc506854cda9b566526b74f25a
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-24 11:38:55 +0300

    Protected Routes

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
 
 from .models import Product
 from .products import products
@@ -49,9 +51,19 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
   user = request.user
   serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+  users = User.objects.all()
+  serializer = UserSerializer(users, many=True)
+  # return JsonResponse(products, safe=False)
   return Response(serializer.data)
 
 
