4b96b1efa119d142c610f52cd359348a69d5af8c
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-05-10 09:54:37 +0300

    Frontend Product Reviews

--- backend/base/views.py
+++ backend/base/views.py
@@ -293,13 +293,19 @@ def createProductReview(request, pk):
     user = request.user
     product = Product.objects.get(_id=pk)
     data = request.data
+
+    # 1 - Review already exists
     alreadyExists = product.review_set.filter(user=user).exists()
     if alreadyExists:
-        content = {'details': 'Product already reviewed'}
+        content = {'detail': 'Product already reviewed'}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # 2 - No Rating or 0
     elif data['rating'] == 0:
-        content = {'details': 'Please select a rating'}
+        content = {'detail': 'Please select a rating'}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # 3 - Create review
     else:
         review = Review.objects.create(
             user=user,
@@ -308,11 +314,15 @@ def createProductReview(request, pk):
             rating=data['rating'],
             comment=data['comment'],
         )
+
         reviews = product.review_set.all()
         product.numReviews = len(reviews)
+
         total = 0
         for i in reviews:
             total += i.rating
-            product.rating = total / len(reviews)
+
+        product.rating = total / len(reviews)
         product.save()
+
         return Response('Review Added')
